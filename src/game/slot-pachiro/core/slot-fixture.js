6b6928b30be5287ee06658bc095d37a23ff3f77a
mth310-sys <mth.310@gmail.com>
2026-09-05 01:19:06 +0900

    Componentize slot fixture parts

---
+++ src/game/slot-pachiro/core/slot-fixture.js
@@ -0,0 +1,17 @@
+export const SLOT_FIXTURE = Object.freeze({
+  cell: 64,
+  shelf: Object.freeze({ width: 64, depth: 34, baseHeight: 20 }),
+  machineSand: Object.freeze({ width: 64, machineWidth: 60, sandWidth: 4, depth: 34, height: 50 }),
+  slumpGraph: Object.freeze({ width: 60, depth: 6, height: 10 }),
+  stool: Object.freeze({ diameter: 25, height: 18 })
+});
+
+export function createSlotFixture(options = {}) {
+  return {
+    type: 'slot-fixture',
+    shelf: options.shelf !== false,
+    machineSand: options.machineSand !== false,
+    slumpGraph: options.slumpGraph !== false,
+    stool: options.stool !== false
+  };
+}
